159afbcb5fc95094f81357cf97f96f0b1c8d1ec1
shmesa22 <shmesa23@gmail.com>
2017-03-31 16:47:21 -0500

    Implementa la funcionalidad eliminar post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+
+    redirect_to posts_path, notice: "El post ha sido eliminado"
   end
 
   private
