1769a5b3e7daaf54c9b36aef48f31b589800ab59
shmesa22 <shmesa23@gmail.com>
2017-04-01 13:58:02 -0500

    Agrega validaciones a users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password validations: false
+
+  validates :email, uniqueness: true, format: /@/
+  validates :password, presence: true, on: :create
+  validates :password, length: { in: 6..20 }, allow_nil: true
 end
