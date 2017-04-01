2bce6fe5c97949f9763de9b863087f57886a75e5
shmesa22 <shmesa23@gmail.com>
2017-04-01 12:34:24 -0500

    Implementa la funcionalidad ingresar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'posts#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   
   resources :users, only: [:new, :create]
   resources :posts
